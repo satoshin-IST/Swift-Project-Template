ef899f5e06ada4b458921bce8d8ce5b4bc050116
satoshin-IST <shin.sato@i-studio.co.jp>
2018-01-19 14:45:39 +0900

    AppDelegateのスケルトンを整理

---
+++ Project-iOS/XLProjectName/XLProjectName/AppDelegate+Configure.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate+Configure.swift
+//  XLProjectName
+//
+//  Created by XLAuthorName 
+//  Copyright © 2018 'XLOrganizationName'. All rights reserved.
+//
+
+import Foundation
+import Fabric
+import Crashlytics
+
+extension AppDelegate {
+
+    func setupCrashlytics() {
+        Fabric.with([Crashlytics.self])
+        Fabric.sharedSDK().debug = Constants.Debug.crashlytics
+    }
+    
+    func setupGoogleAnalytics() {
+        // TODO:
+    }
+    
+}
