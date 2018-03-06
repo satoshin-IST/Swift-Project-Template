94a6db93f3c45328bdbd0780b60e6bb6a07ed525
池野 湧太 <yuta.ikeno@i-studio.co.jp>
2018-03-06 14:48:23 +0900

    Refs #17 Push通知用のモジュールを追加

--- Project-iOS/XLProjectName/XLProjectName/AppDelegate+PushNotification.swift
+++ Project-iOS/XLProjectName/XLProjectName/AppDelegate+PushNotification.swift
@@ -15,19 +15,17 @@ extension AppDelegate {
     }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        PushNotificationModule.shared.didRegisterForRemoteNotificationsWithDeviceToken(application, deviceToken: deviceToken)
     }
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
     }
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-//        if application.applicationState == .active {
-//
-//        } else if application.applicationState == .background || application.applicationState == .inactive {
-//
-//        }
+        PushNotificationModule.shared.didReceiveRemoteNotification(application: application, options: userInfo)
     }
     
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
     }
 }
