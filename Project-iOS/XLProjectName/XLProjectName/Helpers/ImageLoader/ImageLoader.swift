76687c0365e4b4e46ff97dde7c4e25e1c97c060f
satoshin-IST <shin.sato@i-studio.co.jp>
2018-03-29 12:46:52 +0900

    remove Exclusion

--- Project-iOS/XLProjectName/XLProjectName/Helpers/ImageLoader/ImageLoader.swift
+++ Project-iOS/XLProjectName/XLProjectName/Helpers/ImageLoader/ImageLoader.swift
@@ -9,14 +9,26 @@
 import Foundation
 import Nuke
 
-
 class ImageLoader {
     
     static let shared = ImageLoader()
     
+    static var defaultConfig: URLSessionConfiguration {
+        let config = URLSessionConfiguration.default
+        let urlCache = Nuke.DataLoader.sharedUrlCache
+        
+        // `expires → max-age` の順にキャッシュを確認する
+
+        // キャッシュ多用する
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        return config
+    }
+    
 //    let nuke = { () -> Nuke.Manager in
-////
+//
 ////        let loader = Nuke.Loader(loader: NukeAlamofirePlugin.DataLoader())
 ////        return Nuke.Manager(loader: loader, cache: Cache.shared)
 //    }()
+    
+    
 }
