988bc6bb4788d25165ba27ffde2defbbd02c2d84
satoshin-IST <shin.sato@i-studio.co.jp>
2018-03-29 12:08:57 +0900

    add Exclusion , remove Alamofire-Plugin

---
+++ Project-iOS/XLProjectName/XLProjectName/Helpers/ImageLoader/ImageLoader.swift
@@ -0,0 +1,22 @@
+//
+//  ImageLoader.swift
+//  XLProjectName
+//
+//  Created by XLAuthorName.
+//  Copyright © 2018 'XLOrganizationName'. All rights reserved.
+//
+
+import Foundation
+import Nuke
+
+
+class ImageLoader {
+    
+    static let shared = ImageLoader()
+    
+//    let nuke = { () -> Nuke.Manager in
+////
+////        let loader = Nuke.Loader(loader: NukeAlamofirePlugin.DataLoader())
+////        return Nuke.Manager(loader: loader, cache: Cache.shared)
+//    }()
+}
